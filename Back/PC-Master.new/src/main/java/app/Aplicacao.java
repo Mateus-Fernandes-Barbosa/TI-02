2c1be9a6949fc5f88792d2132457349bf7d5afd3
LuisAugusto0 <laloaugusto78@gmail.com>
2023-10-27 09:27:26 -0300

    final v2

--- Back/PC-Master.new/src/main/java/app/Aplicacao.java
+++ Back/PC-Master.new/src/main/java/app/Aplicacao.java
@@ -1,6 +1,8 @@
 package app;
+import service.PecaComp;
 import service.PecaService;
 import service.Template;
+
 
 import static spark.Spark.*;
 
@@ -13,6 +15,8 @@ public class Aplicacao {
 	private static PecaService Ps= new PecaService();
 	private static Template template= new Template();
 	
+	private static PecaComp pecaComp = new PecaComp();
+	
 	public  static void main(String args[]) {
 		port(6789);
 		staticFiles.location("/public");
@@ -23,6 +27,25 @@ public class Aplicacao {
 		post("/produto/update/:id", (request, response) -> Ps.update(request, response));
 		post("/produto/insert", (request, response) -> Ps.insert(request, response));
 		get("/produto/delete/:id", (request, response) -> Ps.delete(request, response));
+		
+		 // Substitua 1 e 2 pelos idsde peças válidos no seu banco de dados
+	       pecaComp.Comparacao(1, 2);
+	       System.out.println("\n---------");
+	       pecaComp.Comparacao(6, 7);
+	       System.out.println("\n---------");
+	       pecaComp.Comparacao(19, 20);
+	       System.out.println("\n---------");
+	       pecaComp.CompatibilidadeProcessadorMotherBoard(1, 11);
+	       System.out.println("\n---------");
+	       pecaComp.CompatibilidadeProcessadorMotherBoard(2, 11);
+	       System.out.println("\n---------");
+	       pecaComp.Compatibilidade(1, 7, 11);
+	       System.out.println("\n---------");
+	       pecaComp.Compatibilidade(2, 6, 13);
+	       System.out.println("\n---------");
+	       pecaComp.Compatibilidade(1, 16, 11);
+	       System.out.println("\n---------");
+	       pecaComp.Compatibilidade(1, 7, 24);
 	}
 }
 
